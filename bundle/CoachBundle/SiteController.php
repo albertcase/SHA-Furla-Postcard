8e2daae85ba6ff7db51922317ae6f56d4df0df45
Demon Zhang <demon.zhang@samesamechina.com>
2016-11-22 14:20:20 +0800

    no message

--- bundle/CoachBundle/SiteController.php
+++ bundle/CoachBundle/SiteController.php
@@ -7,21 +7,21 @@ use Core\Controller;
 class SiteController extends Controller {
 
 	public function indexAction() {
-		$UserAPI = new \Lib\UserAPI();
-		$user = $UserAPI->userLoad(true);
-		if (!$user) {
-			$parameterAry = $_GET;
-			if(count($parameterAry)>0)
-				$url = "/?".http_build_query($parameterAry);
-			else
-				$url = "/";
-			$_SESSION['redirect_url'] = $url;
-			$WechatAPI = new \Lib\WechatAPI();
-			$WechatAPI->wechatAuthorize();
-		}
-		$databaseapi = new \Lib\DatabaseAPI();
-		$type = $databaseapi->checkuser($user->openid);
-		$this->render('site/index', array('type'=>$type));
+		// $UserAPI = new \Lib\UserAPI();
+		// $user = $UserAPI->userLoad(true);
+		// if (!$user) {
+		// 	$parameterAry = $_GET;
+		// 	if(count($parameterAry)>0)
+		// 		$url = "/?".http_build_query($parameterAry);
+		// 	else
+		// 		$url = "/";
+		// 	$_SESSION['redirect_url'] = $url;
+		// 	$WechatAPI = new \Lib\WechatAPI();
+		// 	$WechatAPI->wechatAuthorize();
+		// }
+		// $databaseapi = new \Lib\DatabaseAPI();
+		// $type = $databaseapi->checkuser($user->openid);
+		// $this->render('site/index', array('type'=>$type));
 		exit;
 	}
 
