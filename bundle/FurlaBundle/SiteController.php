1c6f9b0509cd1a474f66526860a7afce81dd1f57
evelyn_zhang <1054982653@qq.com>
2016-11-25 16:38:39 +0800

    merge

--- bundle/FurlaBundle/SiteController.php
+++ bundle/FurlaBundle/SiteController.php
@@ -1,5 +1,5 @@
 <?php
-namespace CoachBundle;
+namespace FurlaBundle;
 
 use Core\Controller;
 
@@ -7,21 +7,22 @@ use Core\Controller;
 class SiteController extends Controller {
 
 	public function indexAction() {
-		// $UserAPI = new \Lib\UserAPI();
-		// $user = $UserAPI->userLoad(true);
-		// if (!$user) {
-		// 	$parameterAry = $_GET;
-		// 	if(count($parameterAry)>0)
-		// 		$url = "/?".http_build_query($parameterAry);
-		// 	else
-		// 		$url = "/";
-		// 	$_SESSION['redirect_url'] = $url;
-		// 	$WechatAPI = new \Lib\WechatAPI();
-		// 	$WechatAPI->wechatAuthorize();
-		// }
-		// $databaseapi = new \Lib\DatabaseAPI();
-		// $type = $databaseapi->checkuser($user->openid);
-		// $this->render('site/index', array('type'=>$type));
+		$UserAPI = new \Lib\UserAPI();
+		$user = $UserAPI->userLoad(true);
+		if (!$user) {
+			$parameterAry = $_GET;
+			if(count($parameterAry)>0)
+				$url = "/?".http_build_query($parameterAry);
+			else
+				$url = "/";
+			$_SESSION['redirect_url'] = $url;
+			$WechatAPI = new \Lib\WechatAPI();
+			$WechatAPI->wechatAuthorize();
+		}
+		var_dump($user);exit;
+		$databaseapi = new \Lib\DatabaseAPI();
+		$type = $databaseapi->checkuser($user->openid);
+		$this->render('site/index', array('type'=>$type));
 		exit;
 	}
 
--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 define("REDIS_HOST", '127.0.0.1');
 define("REDIS_PORT", '6379');
 
-define("BASE_URL", 'http://coachvip.samesamechina.com/');
+define("BASE_URL", 'http://furlasparklesofjoy.samesamechina.com/');
 define("TEMPLATE_ROOT", dirname(__FILE__) . '/../template');
 
 //当前时间
@@ -14,12 +14,12 @@ define("WX_APPID", 'wx737a6d5fe4d19c89');
 //微信商户id
 define("WX_BUSID", '1275055201');
 //Curio接口
-define("CURIO_AUTH_URL", 'http://coach.samesamechina.com/api/wechat/oauth/auth/9b2cea78-4b43-4c04-8f61-b8ebd1601e3b');
-define("CURIO_TOKEN", 'zcBpBLWyAFy6xs3e7HeMPL9zWrd7Xy');
-define("CURIO_JS_ID", '912e9ed6-7426-49c0-98f2-903fa4bf0d7a');
+define("CURIO_AUTH_URL", 'http://oauth.curio.im/v1/wx/web/auth/6572c808-e608-4370-b26a-e08f7b1bd1a6');
+define("CURIO_TOKEN", '91a36024-6ece-43ea-8a98-ee02b802d9f6');
+define("CURIO_JS_ID", 'b205e622-e695-4157-b5d2-eae508c0454a');
 
 //Database config info
 define("DBHOST", '127.0.0.1');
 define("DBUSER", 'root');
 define("DBPASS", '1qazxsw2');
-define("DBNAME", 'coach_vip');
+define("DBNAME", 'furla_postcard');
