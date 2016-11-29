39662ab96658064ba0b6c1080df45d412b503b72
evelyn_zhang <1054982653@qq.com>
2016-11-29 17:19:50 +0800

    gift page

--- assets/js/gift.js
+++ assets/js/gift.js
@@ -1,361 +1,42 @@
 ;(function(){
 
-    var furla = function(){
+    var gift = function(){
 
     };
     //init
-    furla.prototype.init = function(){
+    gift.prototype.init = function(){
         var self = this;
-        //self.welcomePage();
-        //self.writeCard();
-        //self.shareCallback();
-    };
-    //welcome page
-    furla.prototype.welcomePage = function(){
-        var self = this;
-
-        Common.gotoPin(0);
-        self.selectProductPage();
-
-        //popup for rule
-        $('.show-rule').on('click',function(){
-            $('#popup-rule').addClass('show');
-        });
-        $('#popup-rule .btn-close').on('click',function(){
-            $('#popup-rule').removeClass('show');
-        });
-
-        //go select product page
-        $('.arrow-next').on('touchstart',function(){
-            self.selectProductPage();
-        });
-
+        self.openGift();
     };
 
-    //select product page
-    furla.prototype.selectProductPage = function(){
-        var self = this;
-        var swiperHtml = '';
-        Common.gotoPin(1);
-        for(var i=0;i<products.length;i++){
-            swiperHtml = swiperHtml + '<div class="swiper-slide">'+
-                '<div class="item-content">'+
-                '<div class="slide-block" pid="'+products[i].pid+'">'+
-                '<img src="'+products[i].imgsrc+'" alt=""/>'+
-                '</div>'+
-                '</div><div class="product-name">'+products[i].name+'</div>'+
-                '</div>';
+    //open page
+    gift.prototype.openGift = function(){
+        //imulate shake function
+        $('.box-top').on('touchstart',function(){
+            openBox();
+        });
+        openBox();
+        showLetter();
+
+        function openBox(){
+            $('.box-top').addClass('open');
+            $('.pg1-t2').removeClass('hide');
+        //    api
+        //    load products
+            console.log('加载并显示产品');
         }
 
-        $('.swiper-wrapper').html(swiperHtml);
-        var swiper = new Swiper('.swiper-container', {
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-            pagination: '.swiper-pagination',
-            paginationClickable: true,
-            // Disable preloading of all images
-            preloadImages: false,
-            slidesPerView: 3,
-            // Enable lazy loading
-            lazyLoading: true
+        //find the letter card ,and then show
+        $('.dest-block').on('touchstart',function(){
+            showLetter();
         });
 
-        /*
-         * The function is to drag slide products to destionation area
-         *
-         * */
-        function getElementLeft(element){
-            var actualLeft = element.offsetLeft;
-            var current = element.offsetParent;
-            while (current !== null){
-                actualLeft += current.offsetLeft;
-                current = current.offsetParent;
-            }
-            return actualLeft;
+        function showLetter(){
+            $('.card').addClass('goright');
+            $('.section-letter').removeClass('hide');
+            console.log('api for 信纸内容');
         }
-
-        function getElementTop(element){
-            var actualTop = element.offsetTop;
-            var current = element.offsetParent;
-
-            while (current !== null){
-                actualTop += current.offsetTop;
-                current = current.offsetParent;
-            }
-            return actualTop;
-        }
-
-//    drag-block
-        var point, x, y,dx,dy;
-
-        var maxX,maxY,minX,minY;
-
-        /*If move the product success,true*/
-        //var isMoved = false;
-
-        var dragEle = $('.drag-ele');
-        var destEle = $('.dest-block');
-
-        var enableMove = true;
-
-        var curPid;
-        var curIndex;
-        var isFull = false;
-
-        var addHtml;
-        var selectedProducts = ['','',''];
-
-        var isAddProduct = false,
-            isRemoveProduct = false;
-
-        function resetDragElePos(ele){
-            ele.css({
-                'opacity':'0',
-                'left':'-3rem',
-                'top':'-1.8rem',
-                //'z-index':'-1'
-            }).html('');
-        };
-
-        /*If the drag elements is in destination block*/
-        function isBelongDest(posX,posY,minX,minY,maxX,maxY){
-            if( posX < minX || posX > maxX || posY < minY || posY > maxY){
-                //    not belong
-                return false;
-            }
-            return true;
-        }
-
-        function changeStatus(ischange){
-            if(ischange){
-                $('.btn-writecard').removeClass('hide');
-                $('.p2-t1 img').attr('src','/dist/images/p2-t2.png');
-            }else{
-                $('.btn-writecard').addClass('hide');
-                $('.p2-t1 img').attr('src','/dist/images/p2-t1.png');
-            }
-
-        };
-
-
-        function addProducts(){
-            console.log('添加中。。。');
-            for(var i=0;i<selectedProducts.length;i++){
-                if(!selectedProducts[i]){
-                    selectedProducts[i] = curPid;
-                    $('.dest-block .item').eq(i).html(addHtml);
-                    resetDragElePos(dragEle);
-                    if(i==2){
-                        isFull = true;
-                        changeStatus(true);
-                    }
-                    return;
-                }
-            }
-
-
-        };
-
-
-        function removeProducts(index){
-
-            console.log('移除中。。。');
-            //selectedProducts
-            $('.item-dest').eq(index).find('.slide-block').remove();
-            selectedProducts.splice(index,1,'');
-            isFull = false;
-            changeStatus(false);
-        };
-
-
-
-        /*touch start function*/
-        function handlerDragTS(e){
-            enableMove = true;
-            minX = destEle.offset().left;
-            minY = destEle.offset().top;
-            maxX = minX + parseInt(destEle.width());
-            maxY = minY + parseInt(destEle.height());
-            x = e.changedTouches[0].clientX;
-            y = e.changedTouches[0].clientY;
-
-            //add products
-            if($(e.target).parent().hasClass('slide-block') && $(e.target).parent().parent().parent().hasClass('swiper-slide-next') && !isFull){
-                console.log('开始添加');
-                curPid = $(e.target).parent().attr('pid');
-                addHtml = $(e.target).parent().parent().html();
-                dragEle.css({
-                    'opacity':'0',
-                    'left':x + 'px',
-                    'top':y + 'px'
-                });
-                dragEle.html(addHtml);
-                isAddProduct = true;
-                isRemoveProduct = false;
-            };
-
-            if($(e.target).parent().hasClass('slide-block') && $(e.target).parent().parent().hasClass('item-dest')){
-                console.log('开始移除');
-                curPid = $(e.target).parent().attr('pid');
-                curIndex = $(e.target).parent().parent().index();
-                dragEle.css({
-                    'opacity':'0',
-                    'left':x + 'px',
-                    'top':y + 'px'
-                });
-                dragEle.html($(e.target).parent().parent().html());
-                isAddProduct = false;
-                isRemoveProduct = true;
-            };
-
-        };
-
-
-        /*touch move function*/
-        function handlerDragTM(e){
-            dx = e.changedTouches[0].clientX - x;
-            dy = e.changedTouches[0].clientY - y;
-            if(isAddProduct){
-                dragEle.css({
-                    'opacity':'1',
-                    left:e.changedTouches[0].clientX + 'px',
-                    top:e.changedTouches[0].clientY + 'px'
-                });
-
-                if(isBelongDest(e.changedTouches[0].clientX,e.changedTouches[0].clientY,minX,minY,maxX,maxY)){
-                    if(!enableMove) return;
-                    enableMove = false;
-                    addProducts();
-                }
-            }
-            if(isRemoveProduct){
-                console.log('move移除');
-                dragEle.css({
-                    'opacity':'1',
-                    left:e.changedTouches[0].clientX + 'px',
-                    top:e.changedTouches[0].clientY + 'px'
-                });
-                //var dHtml = dragEle.html();
-                if(isBelongDest(e.changedTouches[0].clientX,e.changedTouches[0].clientY,minX,minY,maxX,maxY)){
-
-                }else{
-                    if(!enableMove) return;
-                    enableMove = false;
-                    resetDragElePos(dragEle);
-                    removeProducts(curIndex);
-                }
-            }
-
-
-        };
-
-        /*touch end function*/
-        function handlerDragTE(e){
-            isAddProduct = false;
-            isRemoveProduct = false;
-            curIndex='';
-            addHtml = '';
-            resetDragElePos(dragEle);
-        };
-
-
-        $('.drag-panel').on('touchstart',handlerDragTS);
-        $('.drag-panel').on('touchmove',handlerDragTM);
-        $('.drag-panel').on('touchend',handlerDragTE);
-
-
-        function submitSelectedProduct(){
-            if(isFull){
-                //接口1
-                console.log(selectedProducts);
-                console.log('submit selected products id');
-            }else{
-                console.log('请选择三个产品');
-            }
-
-        };
-
-        $('.btn-writecard').on('touchstart',submitSelectedProduct);
-
     };
-
-    //write card with words
-    furla.prototype.writeCard = function(){
-        Common.gotoPin(2);
-        $('.bg .bg-layer-1').addClass('bg-right');
-
-        //    update date
-        var curDate = new Date();
-        var y = curDate.getFullYear(),
-            m=curDate.getMonth()+1,
-            d=curDate.getDate();
-        var curDay = y+'年'+m+'月'+d+'日';
-        $('.letter-date').html(curDay);
-
-        var letterContent = $('#l-content');
-        var startFocus = false; //if focus first,all init text need disappear
-        var initVal = 'FURLA全新系列带着我最深刻的祝福，\n用绽放的星芒，\n为您装点一个绚烂的惊喜。';
-        var curVal;
-        letterContent.val(initVal);
-        letterContent.on('focus',function(){
-            if(!startFocus){
-                letterContent.val('');
-            }
-            startFocus = true;
-        });
-
-        //function charLimit(limitField, limitCount, limitNum) {
-        //    if (limitField.value.length > limitNum) {
-        //        limitField.value = limitField.value.substring(0, limitNum);
-        //    } else {
-        //        limitCount.value = limitNum - limitField.value.length;
-        //    }
-        //};
-
-        letterContent.on('keydown',function(e){
-            curVal = letterContent.val();
-            if(e.keyCode==13 || e.which==13){
-                var a = curVal.split('\n');
-                if(a.length>2){
-                    return false;
-                }
-            }
-        });
-
-        //send product and words to backend
-        $('.btn-postcard').on('touchstart',function(){
-            curVal = letterContent.val();
-            console.log(curVal);
-            /*here*/
-            //go share page
-            Common.gotoPin(2);
-        //    submit success,do animation
-            doAniForLetter();
-
-        //    start to activate
-            console.log('激活分享');
-
-        });
-
-        function doAniForLetter(){
-            $('.section-letter').addClass('shrinktocorner');
-            $('.box-top').addClass('movetocenter');
-        };
-
-
-    };
-
-    //share callback
-    furla.prototype.shareCallback = function(){
-    //  go pin-4
-        Common.gotoPin(3);
-    //    lucky draw success
-    //    api
-        console.log('');
-
-
-    };
-
 
 
 
@@ -363,7 +44,7 @@
     //dom ready
     $(document).ready(function(){
 
-        var myfurla = new furla();
+        var myfurla = new gift();
         myfurla.init();
 
 
