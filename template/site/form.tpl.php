f744ebc91208aa7f4b4254730e7ccf26bedc8b76
evelyn_zhang <1054982653@qq.com>
2016-12-01 16:38:22 +0800

    merge

---
+++ template/site/form.tpl.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Furla</title>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="full-screen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="/dist/css/style.css" />
+    <script type="text/javascript" src="http://wechatjs.curio.im/api/v1/js/b205e622-e695-4157-b5d2-eae508c0454a/wechat.js"></script>
+    <!--<script type="text/javascript" src="/assets/js/lib/zepto.min.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/rem.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/common.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/wxshare.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/api.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/form.js"></script>-->
+    <script type="text/javascript" src="/dist/js/form_all.min.js"></script>
+</head>
+<body class="page-form">
+<div class="wrapper">
+    <div class="bg">
+        <div class="bg-layer-1"></div>
+        <div class="bg-layer-2"></div>
+    </div>
+    <div class="v-content animate fade">
+        <div class="logo">
+            <img src="/dist/images/logo.png" alt=""/>
+        </div>
+        <form id="form-contact">
+            <h2 class="title"><img src="/dist/images/text-form-tips.png" alt=""/></h2>
+            <div class="form-information">
+                <div class="input-box input-box-name">
+                    <input type="text" id="input-lastname" placeholder="姓..."/>
+                    <input type="text" id="input-firstname" placeholder="名..."/>
+                </div>
+                <div class="input-box input-box-mobile">
+                    <input type="text" disabled id="input-regioncode" placeholder="+86"/>
+                    <input type="tel" maxlength="11" id="input-mobile" placeholder="手机..."/>
+                </div>
+                <div class="input-box input-box-address">
+                    <input type="text" id="input-address" placeholder="地址..."/>
+                </div>
+                <div class="input-box input-box-mail">
+                    <input type="text" id="input-mail" placeholder="邮箱..."/>
+                </div>
+                <div class="input-box input-checkbox">
+                    <input type="checkbox" name="receive" id="input-receive"/>
+                    <label for="input-receive">我愿意收到有关FURLA产品、服务、推广等最新消息。</label>
+                </div>
+            </div>
+            <div class="btn btn-submit"><span>确认</span></div>
+        </form>
+        <div class="success-block hide">
+            <div class="s-t1">
+                <img src="/dist/images/tips-1.png" alt=""/>
+            </div>
+            <div class="btn btn-gohome"><span>我也要送好礼祝福</span></div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+
+</html>
---
+++ template/site/gift.tpl.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Furla</title>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="full-screen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="/dist/css/style.css" />
+    <script type="text/javascript" src="http://wechatjs.curio.im/api/v1/js/b205e622-e695-4157-b5d2-eae508c0454a/wechat.js"></script>
+    <!--<script type="text/javascript" src="/assets/js/lib/zepto.min.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/lib/pre-loader.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/lib/reqAnimate.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/lib/shake.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/rem.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/productsjson.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/common.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/wxshare.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/api.js"></script>-->
+    <!--<script type="text/javascript" src="/assets/js/gift.js"></script>-->
+    <script type="text/javascript" src="/dist/js/gift_all.min.js"></script>
+</head>
+<body class="page-home">
+<div class="wrapper">
+    <div class="bg">
+        <div class="bg-layer-1"></div>
+        <!--<div class="bg-layer-2"></div>-->
+    </div>
+    <div class="preload">
+        <div class="v-content"></div>
+    </div>
+    <div class="box-animate animate">
+        <img src="/dist/images/animate/L_00044.jpg" alt=""/>
+    </div>
+    <div class="container animate">
+        <!-- shake page-->
+        <section class="pin pin-gift-1 ">
+            <div class="logo">
+                <img src="/dist/images/logo.png" alt=""/>
+            </div>
+            <div class="content">
+                <div class="pg1-t1">
+                    <img src="/dist/images/t-open.png" alt=""/>
+                </div>
+            </div>
+            <div class="box-bottom animate">
+                <div class="dest-block-wrap">
+                    <div class="dest-block">
+                        <!--<div class="item item-dest item-1">-->
+                            <!--<img src="/dist/images/products/p1.png" alt=""/>-->
+                        <!--</div>-->
+                        <!--<div class="item item-dest item-2">-->
+                            <!--<img src="/dist/images/products/p2.png" alt=""/>-->
+                        <!--</div>-->
+                        <!--<div class="item item-dest item-3">-->
+                            <!--<img src="/dist/images/products/p3.png" alt=""/>-->
+                        <!--</div>-->
+                    </div>
+                    <div class="card animate">
+                        <img src="/dist/images/card.png" alt=""/>
+                    </div>
+                </div>
+                <div class="pg1-t2 hide">
+                    <img src="/dist/images/text-3.png" alt=""/>
+                </div>
+            </div>
+        </section>
+        <section class="pin pin-gift-2">
+            <!-- 信件-->
+            <div class="section-letter animate delay1 fade hide">
+                <div class="logo">
+                    <img src="/dist/images/logo.png" alt=""/>
+                </div>
+                <div class="bg-layer-2"></div>
+                <div class="letter-wrap">
+                    <div class="letter-header">
+                        <label>敬爱的</label>
+                        <input type="text" id="input-name-1"/>
+                    </div>
+                    <div class="letter-content">
+                        <!--FURLA全新系列带着我最深刻的祝福，-->
+                        <!--用绽放的星芒，-->
+                        <!--为您装点一个绚烂的惊喜。-->
+                        <textarea name="l-content" id="l-content"  cols="30" rows="3" wrap="hard" maxlength="60"></textarea>
+                        <!--<div contenteditable="true" class="test" style="font-size: 0.26rem;line-height: 2">敬爱的</div>-->
+                    </div>
+                    <div class="letter-bottom">
+                        <label>敬爱的</label>
+                        <input type="text" id="input-name-2"/>
+                    </div>
+                    <div class="letter-date">2016年11月24日</div>
+                </div>
+                <div class="btn btn-postcard"><span>参与抽奖</span></div>
+                <div class="box-top animate open"></div>
+            </div>
+        </section>
+        <!-- 中奖之后-->
+        <section class="pin pin-gift-3 current">
+            <div class="logo">
+                <img src="/dist/images/logo.png" alt=""/>
+            </div>
+            <div class="replace-text rt-1">
+                <img src="/dist/images/text-key-1.png" alt=""/>
+            </div>
+            <div class="btn btn-goform"><span>填写联系方式领取大奖</span></div>
+        </section>
+
+    </div>
+
+
+</div>
+
+
+</body>
+
+
+</html>
