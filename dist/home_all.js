7b35b03157b48288391c56a26dc32f3c2a60d702
evelyn_zhang <1054982653@qq.com>
2016-12-05 12:06:32 +0800

    share baidu

--- dist/home_all.js
+++ dist/home_all.js
@@ -795,7 +795,7 @@ $(document).ready(function(){
                 dataUrl: '',
                 success: function () {
                     //    success
-                    _hmt.push(['_trackEvent', 'wxshare', 'share', 'share']);
+                    _hmt.push(['_trackEvent', 'btn-weixin', 'share', 'success']);
                     callback();
 
                 },
@@ -807,6 +807,7 @@ $(document).ready(function(){
                 link: obj.link,
                 imgUrl: obj.img,
                 success: function () {
+                    _hmt.push(['_trackEvent', 'btn-weixin', 'share', 'success']);
                     callback();
                 },
                 cancel: function () {
